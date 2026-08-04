42ceb0f2e349255a31bc79fa17b2b1c43ce4f96b
franck34300 <lebihan.agde@gmail.com>
2026-08-04 13:40:56 +0200

    Enhance Vite config with server and build settings
    Added configuration for path aliasing, server, preview, and build options.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  server: {
+    host: true,
+    port: 5173,
+  },
+
+  preview: {
+    host: true,
+    port: 4173,
+  },
+
+  build: {
+    sourcemap: false,
+    target: 'esnext',
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+})
